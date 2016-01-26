a29995115f3d7308936cdb241a453f4b09823695
saharati90 <sahar.harati@emory.edu>
2016-01-26 10:56:27 -0500

    some edits

--- CCLE/CCLEBuildPathwaysTable.m
+++ CCLE/CCLEBuildPathwaysTable.m
@@ -83,7 +83,9 @@ pathway = zeros(length(Source),length(lines));
 labels = cell(length(Source),length(lines));
 
 for k = 1:length(lines)
+    k
     for j = 1:length(Source)
+        j
         [~, ind_1] = ismember(Source{j}, mut_genes);
         s_m = '';
         if (ind_1 ~= 0)
