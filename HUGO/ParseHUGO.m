33a4e6c2cf11fe28a9423fe895c087c0f3311eb3
Lee Cooper <lee.cooper@emory.edu>
2016-04-28 22:32:22 -0400

    Update ParseHUGO.m
    Added license to header.

--- HUGO/ParseHUGO.m
+++ HUGO/ParseHUGO.m
@@ -12,8 +12,25 @@ function HUGO = ParseHUGO(HUGOFile)
 %       function to determine if a given string is a gene symbol or associated 
 %       with a gene symbol.
 
+%Licensed to the Apache Software Foundation (ASF) under one
+%or more contributor license agreements.  See the NOTICE file
+%distributed with this work for additional information
+%regarding copyright ownership.  The ASF licenses this file
+%to you under the Apache License, Version 2.0 (the
+%"License"); you may not use this file except in compliance
+%with the License.  You may obtain a copy of the License at
+%
+%  http://www.apache.org/licenses/LICENSE-2.0
+%
+%Unless required by applicable law or agreed to in writing,
+%software distributed under the License is distributed on an
+%"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%KIND, either express or implied.  See the License for the
+%specific language governing permissions and limitations
+%under the License.
+
 %open HUGO file
-Contents = text2cell('hgnc_complete_set.txt', '\t');
+Contents = text2cell(HUGOFile, '\t');
 
 %copy header, erase
 Header = Contents(1,:);
