e2b58d8f8eb9d9f20d4a72d92e1407d66f7b443d
Lee Cooper <lee.cooper@emory.edu>
2016-04-28 22:30:28 -0400

    Update CCLEDrugResponse.m
    Added license to header.

--- CCLE/CCLEDrugResponse.m
+++ CCLE/CCLEDrugResponse.m
@@ -19,6 +19,23 @@ function CCLEDrugResponse(DrugFile, LineFile, HUGOFile)
 %						CCLE.Amax - MxN matrix of activation function maximum values
 %						CCLE.AUC - MxN matrix of activation function area-under-curve values
 
+%Licensed to the Apache Software Foundation (ASF) under one
+%or more contributor license agreements.  See the NOTICE file
+%distributed with this work for additional information
+%regarding copyright ownership.  The ASF licenses this file
+%to you under the Apache License, Version 2.0 (the
+%"License"); you may not use this file except in compliance
+%with the License.  You may obtain a copy of the License at
+%
+%  http://www.apache.org/licenses/LICENSE-2.0
+%
+%Unless required by applicable law or agreed to in writing,
+%software distributed under the License is distributed on an
+%"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%KIND, either express or implied.  See the License for the
+%specific language governing permissions and limitations
+%under the License.
+
 %load CCLE descriptions
 Descriptions = text2cell(LineFile, '\t');
 
