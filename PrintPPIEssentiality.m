d5d81dd405b3ab658f277bcc9f1381b456242d01
Lee Cooper <lee.cooper@emory.edu>
2016-04-28 22:28:31 -0400

    Update PrintPPIEssentiality.m
    Added license to header.

--- PrintPPIEssentiality.m
+++ PrintPPIEssentiality.m
@@ -18,6 +18,23 @@ function Table = PrintPPIEssentiality(Essentialities, Source, Target,...
 %Table - M+2 x N+4 cell array of strings that can be written to disk
 %           using PrintTable.m.
 
+%Licensed to the Apache Software Foundation (ASF) under one
+%or more contributor license agreements.  See the NOTICE file
+%distributed with this work for additional information
+%regarding copyright ownership.  The ASF licenses this file
+%to you under the Apache License, Version 2.0 (the
+%"License"); you may not use this file except in compliance
+%with the License.  You may obtain a copy of the License at
+%
+%  http://www.apache.org/licenses/LICENSE-2.0
+%
+%Unless required by applicable law or agreed to in writing,
+%software distributed under the License is distributed on an
+%"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%KIND, either express or implied.  See the License for the
+%specific language governing permissions and limitations
+%under the License.
+
 %get dimensions of input 'Essentialities'
 M = length(Source); %number of PPIs
 N = length(Lines); %number of cell lines
